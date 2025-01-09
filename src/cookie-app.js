9c4c57c1ac933bb7d1dab6ba5bd7f10072cd21bd
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-01-09 11:18:16 +0400

    Update cookie-app.js

--- src/cookie-app.js
+++ src/cookie-app.js
@@ -1,54 +1,55 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (!getCookie('cookieConsent')) {
-        createCookieConsentBanner();
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (!getCookiePopup("cookieConsent")) {
+    createCookieConsentBanner();
+  }
 });
 
 function createCookieConsentBanner() {
-    // Create the outer container div
-    const ambCookieContainer = document.createElement('div');
-    ambCookieContainer.className = 'amb-cookie-container';
+  // Create the outer container div
+  const ambCookieContainer = document.createElement("div");
+  ambCookieContainer.className = "amb-cookie-container";
 
-    // Create the banner div
-    const cookieConsent = document.createElement('div');
-    cookieConsent.className = 'cookieConsent';
+  // Create the banner div
+  const cookieConsent = document.createElement("div");
+  cookieConsent.className = "cookieConsent";
 
-    // Create the container div
-    const container = document.createElement('div');
-    container.className = 'cookieConsentContainer';
+  // Create the container div
+  const container = document.createElement("div");
+  container.className = "cookieConsentContainer";
 
-    // Create the message paragraph
-    const message = document.createElement('p');
-    message.innerHTML = 'საიტი იყენებს Cookie ფაილებს. დახურვის ღილაკზე დაჭერით თქვენ ეთანხმებით Cookie ფაილების გამოყენების წესებს და პირობებს. <a href="https://www.ambassadoribet.com/cookie-policy" target="_blank">Cookie პოლიტიკა.</a>';
+  // Create the message paragraph
+  const message = document.createElement("p");
+  message.innerHTML =
+    'საიტი იყენებს Cookie ფაილებს. დახურვის ღილაკზე დაჭერით თქვენ ეთანხმებით Cookie ფაილების გამოყენების წესებს და პირობებს. <a href="https://www.ambassadoribet.com/cookie-policy" target="_blank">Cookie პოლიტიკა.</a>';
 
-    // Create the accept button
-    const button = document.createElement('button');
-    button.className = 'acceptCookies';
-    button.textContent = 'დახურვა';
-    button.addEventListener('click', () => {
-        setCookie('cookieConsent', 'true', 365);
-        ambCookieContainer.style.display = 'none';
-    });
+  // Create the accept button
+  const button = document.createElement("button");
+  button.className = "acceptCookies";
+  button.textContent = "დახურვა";
+  button.addEventListener("click", () => {
+    setCookie("cookieConsent", "true", 365);
+    ambCookieContainer.style.display = "none";
+  });
 
-    // Append elements to the container
-    container.appendChild(message);
-    container.appendChild(button);
+  // Append elements to the container
+  container.appendChild(message);
+  container.appendChild(button);
 
-    // Append container to the banner
-    cookieConsent.appendChild(container);
+  // Append container to the banner
+  cookieConsent.appendChild(container);
 
-    // Append banner to the outer container
-    ambCookieContainer.appendChild(cookieConsent);
+  // Append banner to the outer container
+  ambCookieContainer.appendChild(cookieConsent);
 
-    // Append outer container to the body
-    document.body.appendChild(ambCookieContainer);
+  // Append outer container to the body
+  document.body.appendChild(ambCookieContainer);
 
-    // Add CSS styles
-    addCookieConsentStyles();
+  // Add CSS styles
+  addCookieConsentStyles();
 }
 
 function addCookieConsentStyles() {
-    const styles = `
+  const styles = `
         .amb-cookie-container {
             position: fixed;
             bottom: 10px;
@@ -109,31 +110,31 @@ function addCookieConsentStyles() {
             bottom: 0px;
         }
     `;
-    const styleSheet = document.createElement('style');
-    styleSheet.id = 'style-cookie';
-    styleSheet.type = 'text/css';
-    styleSheet.innerText = styles;
-    document.head.appendChild(styleSheet);
+  const styleSheet = document.createElement("style");
+  styleSheet.id = "style-cookie";
+  styleSheet.type = "text/css";
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
 }
 
 function setCookie(name, value, days) {
-    const d = new Date();
-    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  const d = new Date();
+  d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1, c.length);
-        }
-        if (c.indexOf(nameEQ) === 0) {
-            return c.substring(nameEQ.length, c.length);
-        }
+function getCookiePopup(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") {
+      c = c.substring(1, c.length);
     }
-    return null;
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
 }
