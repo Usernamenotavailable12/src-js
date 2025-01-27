b0b1a1bdc6f226081b7eb9b20f7563d5192a16fb
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-01-27 13:06:10 +0400

    Update hotGamesBadge.js

--- src/hotGamesBadge.js
+++ src/hotGamesBadge.js
@@ -62,23 +62,24 @@ function generateRecentWinsCSS(wins) {
   ${selectorList} {
     position: absolute;
     color: #ffffff;
-    font-size: 130%;
+    font-size: 140%;
     text-shadow: rgb(0,0,0) 2px 2px;
-    box-shadow: inset 0px 0px 10px black, inset 0px 0px 20px rgba(0,0,0,0.5);
-    background-image: url('https://images.takeshape.io/5da2b4d5-59f6-412a-82c3-f6a272b532be/dev/fcd40f3a-7c06-4d55-ad2b-9eb40ee310d5/Asset%201.svg') !important;
-    background-size: 600% 600%; /* Adjusted so only X-axis moves */
-    background-color: #680053;
+    box-shadow: inset 0px 0px 10px black, inset 0px 0px 10px rgba(0,0,0,0.1);
+    background-image: radial-gradient(#2700af,#11004f) !important;
     bottom: 0;
     left: 0;
-    border-top-right-radius: 10px;
-    border-bottom-left-radius: 7px;
+    border-top-right-radius: 5px;
+    border-bottom-left-radius: 5px;
     padding: 7px;
     z-index: 100000;
-    border-top: solid 4px #1f051c;
-    border-right: solid 4px #1f051c;
+    border-width: 3px;
+    border-style: solid;
+    border-image: linear-gradient(45deg, #ffba2c, #ffea00, #ffba2c, #ffea00, rgb(255,186,45)) 1;
+    outline: solid 4px rgb(31 5 28);
+    border-bottom: none;
+    border-left: none;
     font-family: 'Lilita One' !important;
     pointer-events: none !important;
-    animation: moveBackgroundX 40s ease-in-out infinite; /* Apply animation */
 
     @media (max-width: 768px) {
         font-size: 100%;
