686d899d3945c5dfced39e4b9f8382a23c29beba
Guram Dzneladze <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-02-10 15:06:37 +0400

    Delete src/switchCurrnetUrl-app.js

--- src/switchCurrnetUrl-app.js
+++
@@ -1,8 +0,0 @@
-function switchCurrnetUrl(navurl) {
-  let url = window.location.href;
-  let updatedUrl = url.replace(/(\/(ka|ru|en|tr))(\/.*)?$/, `$1/${navurl}`);
-
-  if (url !== updatedUrl) {
-      window.location.href = updatedUrl; 
-  }
-}
