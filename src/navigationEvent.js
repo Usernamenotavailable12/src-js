1dd74ccab34bb4d68581742864c0f4b62f6db0d8
Guram Dzneladze <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-03-03 16:13:43 +0400

    Create navigationEvent.js

---
+++ src/navigationEvent.js
@@ -0,0 +1,28 @@
+function logCurrentPath(path) {
+    switch (true) {
+        case path.includes('leaderboard'):
+            console.log('hellow world!');
+            break;
+    }
+}
+
+window.onpopstate = () => logCurrentPath(window.location.pathname);
+
+(function(history) {
+    const pushState = history.pushState;
+    const replaceState = history.replaceState;
+
+    history.pushState = function(...args) {
+        const result = pushState.apply(this, args);
+        logCurrentPath(window.location.pathname);
+        return result;
+    };
+
+    history.replaceState = function(...args) {
+        const result = replaceState.apply(this, args);
+        logCurrentPath(window.location.pathname);
+        return result;
+    };
+})(window.history);
+
+logCurrentPath(window.location.pathname);
