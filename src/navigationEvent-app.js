d18aa2c98fc8fab68a0b36d373db9df4cc0bd7ea
Guram Dzneladze <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-03-04 19:14:12 +0400

    Update navigationEvent-app.js

--- src/navigationEvent-app.js
+++ src/navigationEvent-app.js
@@ -1,5 +1,7 @@
 function logCurrentPath(path) {
     setTimeout(() => {
+        let attempts = 0;
+        const maxAttempts = 10;
         const waitForElement = () => {
             if (document.getElementById('tournamentData')) {
                 try {
@@ -11,7 +13,12 @@ function logCurrentPath(path) {
                     setTimeout(() => logCurrentPath(path), 1000); // Retry after 1 second
                 }
             } else {
-                setTimeout(waitForElement, 100); // Check again after 100ms
+                attempts++;
+                if (attempts < maxAttempts) {
+                    setTimeout(waitForElement, 300); // Check again after 100ms
+                } else {
+                    console.error("logCurrentPath: Maximum attempts reached waiting for tournamentData.");
+                }
             }
         };
         waitForElement();
